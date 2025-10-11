a4ed4508abd0c7dabb180f09e066ac34090dec92
Артём Волков <volkov_artyom@mail.ru>
2025-10-11 20:36:44 +0300

    veriosn 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        new Descriptor().construct();
+        new Descriptor().construct(); //main run
     }
 }
